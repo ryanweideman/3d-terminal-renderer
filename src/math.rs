684075239e55f82420857ecd4d5838aabb034546
Ryan Weideman <ryandweideman@gmail.com>
2024-02-18 23:42:05 -0800

    Fix assert

--- src/math.rs
+++ src/math.rs
@@ -5,7 +5,7 @@ pub fn round_up_to_nearest_increment(value: f64, increment: f64) -> f64 {
 }
 
 pub fn scale_range(x: f64, old_min: f64, old_max: f64, new_min: f64, new_max: f64) -> f64 {
-    //assert!(x >= old_min && x <= old_max, "x must be within [-1, 1]");
+    assert!(x >= old_min && x <= old_max, "x must be within [{}, {}]", old_min, old_max);
 
     new_min + ((x - old_min) / (old_max - old_min)) * (new_max - new_min)
 }
