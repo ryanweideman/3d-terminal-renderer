bfcdfe472c443fbb63e11f7a2900fc4aa06a3823
Ryan Weideman <ryandweideman@gmail.com>
2024-11-27 16:26:16 -0800

    Refactor model store

--- src/models/model_store.rs
+++ src/models/model_store.rs
@@ -3,11 +3,18 @@ use crate::models::json_model_loader;
 use crate::models::obj_model_loader;
 
 use include_dir::Dir;
+use include_dir::File;
 use std::collections::HashMap;
 
 pub struct ModelStore<'a> {
     models: HashMap<String, Model>,
     dir: &'a Dir<'a>,
+}
+
+struct FileInfo<'b> {
+    pub file_name: &'b str,
+    pub file_type: &'b str,
+    pub file_contents: &'b str,
 }
 
 impl<'a> ModelStore<'a> {
@@ -19,30 +26,27 @@ impl<'a> ModelStore<'a> {
     }
 
     pub fn init(&mut self) {
-        for file in self.dir.files() {
-            let path = file.path();
+        // Process json files first
+        self.dir
+            .files()
+            .flat_map(|file| get_file_info(file))
+            .filter(|info| info.file_type == "json")
+            .for_each(|info| {
+                let model_geometry = json_model_loader::load_model(info.file_contents);
+                self.models
+                    .insert(info.file_name.to_string(), model_geometry);
+            });
 
-            let file_type = path.extension().and_then(|ext| ext.to_str());
-            let file_name = path
-                .file_name()
-                .and_then(|name| name.to_str())
-                .unwrap()
-                .to_string();
-            let file_contents = file.contents_utf8().expect("Failed to read file contents");
-
-            match file_type {
-                Some("json") => {
-                    let model_geometry = json_model_loader::load_model(file_contents);
-                    self.models.insert(file_name, model_geometry);
-                }
-                Some("obj") => {
-                    let model_geometry = obj_model_loader::load_model(file_contents);
-                    self.models.insert(file_name, model_geometry);
-                }
-                Some(_other) => {}
-                None => {}
-            }
-        }
+        // Process obj files second
+        self.dir
+            .files()
+            .flat_map(|file| get_file_info(file))
+            .filter(|info| info.file_type == "obj")
+            .for_each(|info| {
+                let model_geometry = obj_model_loader::load_model(info.file_contents);
+                self.models
+                    .insert(info.file_name.to_string(), model_geometry);
+            });
     }
 
     pub fn get_model(&self, model_name: &str) -> &Model {
@@ -50,4 +54,16 @@ impl<'a> ModelStore<'a> {
             .get(model_name)
             .unwrap_or_else(|| panic!("Could not get model of name {}", model_name))
     }
+}
+
+fn get_file_info<'b>(file: &'b File<'b>) -> Option<FileInfo<'b>> {
+    let file_type = file.path().extension()?.to_str()?;
+    let file_name = file.path().file_name()?.to_str()?;
+    let file_contents = file.contents_utf8()?;
+
+    Some(FileInfo {
+        file_name,
+        file_type,
+        file_contents,
+    })
 }
